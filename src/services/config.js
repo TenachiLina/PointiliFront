657c6d1872b9dcea117f1b4044c2764a67bd96c0
Nouha Kassama <kassamanouha2004@gmail.com>
2025-11-23 19:46:50 +0100

    YourBackend

--- src/services/config.js
+++ src/services/config.js
@@ -1,3 +1,3 @@
 // export const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api`;
-const API_BASE_URL = "https://your-backend.onrender.com";
+const API_BASE_URL = "https://pointiliback.onrender.com/";
 
